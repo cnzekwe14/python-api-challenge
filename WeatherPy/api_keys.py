1422aa4b1ca29bf48f655bfce1d7fa82278e9291
cnzekwe14 <cnzekwe14@yahoo.com>
2022-11-19 10:18:19 -0500

    add to git

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "7069ec49a45c56e02bd871502946cfa2"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "26235ad084284856a3d9670a5d121672"
